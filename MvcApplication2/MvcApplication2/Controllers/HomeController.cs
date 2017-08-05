0856b1e6ff571529d0ede2a727711141026b76a1
unknown <pt816@CSLAB119.CSSERVER.local>
2017-08-05 19:15:15 +0800

    first revision

---
+++ MvcApplication2/MvcApplication2/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication2.Models;
+
+namespace MvcApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            Information i = new Information();
+            return View(i.Accounts);
+        }
+
+        public ActionResult Add()
+
+        {
+            return View();
+        }
+
+        public ActionResult Save(Account acc)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MvcApplication2/MvcApplication2/Models/information.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication2.Models
+{
+    public class Information
+    {
+      public   List<Account> Accounts;
+
+        public Information()
+        {
+            Accounts = new List<Account>();
+            Accounts.Add(new Account { Id = 4, Name = "anin ", Age = 6 });
+            Accounts.Add(new Account { Id = 1, Name = "putong ", Age = 6 });
+            Accounts.Add(new Account { Id = 3, Name = "johannes ", Age = 6 });
+            Accounts.Add(new Account { Id = 2, Name = "guay ", Age = 6 });
+        }
+    }
+}
